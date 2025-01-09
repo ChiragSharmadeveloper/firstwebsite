d500059bc114d8bb1b5bc0d1eb1add0ee3dbba94
ChiragShamradeveloper <chiragsharmadeveloper@gmail.com>
2025-01-09 23:19:15 +0530

    header update

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+     <div className="App">
+      <header className="App-header">
+       <h1>BBS</h1>
+       <image src='./logo.pdf.jpg'></image>
+       <ul>
+        <li>HOME</li>
+        <li>SUPPORT</li>
+        <li>ABOUT</li>
+        <li>CONTACT US</li>
+       </ul>      
+      </header>
+    </div>
+    </>
+  );
+}
+
+export default App;
