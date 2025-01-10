305296c6bceb527125c405a1a6e8e5c5cf9698b5
ChiragShamradeveloper <chiragsharmadeveloper@gmail.com>
2025-01-10 18:53:56 +0530

    header edit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react' 
-
+import"./index.css"
 
 function App() {
   return (
@@ -7,15 +7,17 @@ function App() {
      <div className="App">
       <header className="App-header">
        <h1>BBS</h1>
-      <img src="/image/logo.pdf.jpg" alt='image1'
-      height={150}
-      width={150}></img>
-
+      <img src="/image/logo.pdf.jpg" alt='image1' id='foto'></img>
        <ul>
-        <li>HOME</li>
-        <li>SUPPORT</li>
-        <li>ABOUT</li>
-        <li>CONTACT US</li>
+        <li class="dropdown">
+        <a href="#">Menu</a>
+        <div class="dropdown-content">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact Us</a>
+          <a href="#">Support</a>
+        </div>
+      </li>
        </ul>      
       </header>
     </div>
