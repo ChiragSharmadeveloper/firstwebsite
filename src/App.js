fec2d9fab3b64aeebe14fd00e7d64c16e5ee8868
abhaypratapdeveloper <abhaypratapdeveloper@gmail.com>
2025-02-07 18:29:47 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -23,12 +23,10 @@ function App() {
             {/* <li><Link href="/about">About</Link></li>
             <li><Link href="/products">Products</Link></li> */}
           </ul>
-<<<<<<< HEAD
+
           </div>
-=======
-</div>
->>>>>>> 5b0e94c8fe32680276ba0d72c471d97140fc9c80
-        
+          
+</div>        
 
     <Banner title={"BB Sports: Your Ultimate Destination for Cricket Gear "} urlimage={"/image/transperent logo.png"}/> 
   <VideoPlayer/>
