13c72a003a0513b705dd2e3df36f343ef0ee8a66
abhaypratapdeveloper <abhaypratapdeveloper@gmail.com>
2025-02-07 16:30:50 +0530

    edit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import About from './about/About';
-import HomePage from './home/Home';
-import ProductPage from './products/Product';
 import Banner from './Banner';
 import VideoPlayer from './Video';
 import Footer from './Footer';
 import"./index.css";
+// import Link from 'next/link';
+import { Link } from 'react-router-dom';
+
 
 function App() {
   return (
@@ -17,24 +16,13 @@ function App() {
       <img src="/image/white.png" alt='image1' id='foto'></img> 
       <i class="fa-solid fa-user" id='login'></i>
       </header>
-      <Router>
       <div className="header2">
           
           <ul>
-          <li><Link to="/home" target="_blank">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/products">Products</Link></li>
+          <li>Home</li>
+            {/* <li><Link href="/about">About</Link></li>
+            <li><Link href="/products">Products</Link></li> */}
           </ul>
-
-          <Routes>
-          <Route path="/home" element={<HomePage/>} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<ProductPage />} />
-        </Routes>
-
-        </div>
-
-        </Router>
         
 
     <Banner title={"BB Sports: Your Ultimate Destination for Cricket Gear "} urlimage={"/image/logo.pdf.jpg"}/> 
@@ -42,6 +30,8 @@ function App() {
 <Footer/>
 </div>
 
+
+</div>
 
     </>
   );
