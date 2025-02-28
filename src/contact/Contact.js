9fdae594e8140afb7cd987d516187c1b68a0233b
ChiragShamradeveloper <chiragsharmadeveloper@gmail.com>
2025-02-28 19:03:57 +0530

    final edits

---
+++ src/contact/Contact.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { FaFacebook } from "react-icons/fa";
+// import { ImTwitter } from "react-icons/im";
+import { FaInstagram } from "react-icons/fa6";
+// import { FaLinkedinIn } from "react-icons/fa";
+import { AiOutlineYoutube } from "react-icons/ai";
+
+const Contact = () => {
+  return (
+    <>
+    <div className="cont">
+
+     <ul>
+              <h2>BB SPORTS SPACE</h2>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3490.8023066920205!2d77.7392802!3d28.963587600000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390c65d68c3aa015%3A0x8ae8db9587cb4db9!2sBabita%20Bhardwaj%20sports!5e0!3m2!1sen!2sin!4v1739364491253!5m2!1sen!2sin" width="100" height="50" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"/>
+            <li><a href="https://maps.app.goo.gl/BoGFN4ZyMZN8rHJ66" target="_blank">GUJRAT , BHAVNAGAR</a></li>
+            <li><a href="/" target="_blank">NEW DELHI , CHIRAG DELHI</a></li>
+            <li><a href="/" target="_blank">COMING SOON....</a></li>
+        </ul>
+        </div>
+
+
+
+
+         <a href='https://www.facebook.com/share/1AJjxYevrE/?mibextid=wwXIfr' target="_blank" className=" my-icon"><i><FaFacebook /> FACEBOOK</i></a>
+                     {/* <i className=" my-icon"><ImTwitter /> TWITTER</i> */}
+                    <a href="https://www.instagram.com/babita_sports?igsh=MTBydXR0NzEzNHhhOQ==" target="_blank" className=" my-icon"><i ><FaInstagram /> INSTAGRAM</i></a>
+                    {/* <i className=" my-icon"><FaLinkedinIn /> LINKDIN</i> */}
+                    <a href="https://youtube.com/@babitasports?si=BeoSVuQIT6tjKiiJ" target="_blank" className=" my-icon"><i><AiOutlineYoutube /> YOUTUBE</i></a>
+    </>
+  )
+}
+
+export default Contact
