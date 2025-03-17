0e5357f0b7b3711f58ff2c5a54463ff37d264bd4
abhaypratapdeveloper <abhaypratapdeveloper@gmail.com>
2025-03-17 16:38:28 +0530

    homepage

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <>
     <header className="App-header">
-          <h1><a href="/App.js" id='bbs'>BBS</a></h1>   
+          <h1><a href="/" id='bbs'>BBS</a></h1>   
           <img src="/image/white.png" alt='image1' id='foto'></img> 
           <i class="fa-solid fa-user" id='login'></i>
           </header>
