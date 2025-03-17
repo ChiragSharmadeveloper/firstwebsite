7b03b0098370ce278ee8dfc8373dc6f5b5992860
abhaypratapdeveloper <abhaypratapdeveloper@gmail.com>
2025-03-17 18:14:22 +0530

    edit

--- src/App.js
+++ src/App.js
@@ -43,7 +43,6 @@ function  App() {
 <Header/>
 <Navbar/>
 <Routes>
-<Route path="/" element={<Header/>}/>
  <Route path="/" element={<Banner/>}/>
   <Route path="/about" element={<About/>} />
   <Route path="/contact" element={<Contact/>} />
