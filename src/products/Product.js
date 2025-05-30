4fc7f05ad980437bfb667e5189b50779b9dd7123
abhaypratapdeveloper <abhaypratapdeveloper@gmail.com>
2025-05-30 18:45:21 +0530

    product

--- src/products/Product.js
+++ src/products/Product.js
@@ -4,24 +4,25 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Navigation, Pagination, A11y } from 'swiper/modules';
 import './Product.css'
+import { SlArrowLeft } from "react-icons/sl";
+import { SlArrowRight } from "react-icons/sl";
 
 const ProductPage = () => {
   
-  
   return (
     <>
+     <button class='lbtn1' ><SlArrowLeft /> </button>
        <Swiper
       
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      modules={[Navigation, Pagination, A11y]}
       spaceBetween={50}
       slidesPerView={1}
        navigation={
         {
-        nextEl: '.rightbtn',
-        prevEl: '.leftbtn'
+        nextEl: '.rbtn1',
+        prevEl: '.lbtn1'
       }}
       freeMode={true}
         pagination={
@@ -30,13 +31,18 @@ const ProductPage = () => {
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
+      <div className='main'>
+        
+      <SwiperSlide>
+        <img alt='slide1'/>
+      </SwiperSlide>
       <SwiperSlide>Slide 2</SwiperSlide>
       <SwiperSlide>Slide 3</SwiperSlide>
       <SwiperSlide>Slide 4</SwiperSlide>
-      1 2 3 . .
+      </div>
     </Swiper>
-     
+      <button class='rbtn1'><SlArrowRight /></button>
+
     </>
   );
 };
