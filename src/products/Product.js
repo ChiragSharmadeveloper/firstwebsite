a673fcb86505d12854f5375779ffa858a0f4ec31
ChiragShamradeveloper <chiragsharmadeveloper@gmail.com>
2025-05-30 19:28:37 +0530

    push

--- src/products/Product.js
+++ src/products/Product.js
@@ -1,28 +1,24 @@
 import React from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Navigation, Pagination, A11y } from 'swiper/modules';
 import './Product.css'
-<<<<<<< HEAD
 import { SlArrowLeft } from "react-icons/sl";
 import { SlArrowRight } from "react-icons/sl";
-=======
-import img1 from'';
->>>>>>> 41e3c39bb02e96c6b737d79f046004cb5b41cf06
+
 
 const ProductPage = () => {
   
   return (
-    <>
-     <button class='lbtn1' ><SlArrowLeft /> </button>
+    <div>
+ <button class='lbtn1' ><SlArrowLeft /> </button>
+      <button class='rbtn1'><SlArrowRight /></button>
        <Swiper
-      
       modules={[Navigation, Pagination, A11y]}
       spaceBetween={50}
-      slidesPerView={1}
+      slidesPerView={2}
        navigation={
         {
         nextEl: '.rbtn1',
@@ -33,28 +29,23 @@ const ProductPage = () => {
           { clickable: true }
         }
       onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-<<<<<<< HEAD
-      <div className='main'>
-        
+      onSlideChange={() => console.log('slide change')}>
+      <div class='main'>
       <SwiperSlide>
-        <img alt='slide1'/>
+        slide 1
       </SwiperSlide>
       <SwiperSlide>Slide 2</SwiperSlide>
       <SwiperSlide>Slide 3</SwiperSlide>
       <SwiperSlide>Slide 4</SwiperSlide>
       </div>
-=======
-      <SwiperSlide><h1>Slide 1</h1></SwiperSlide>
-      <SwiperSlide><h1>Slide 1</h1></SwiperSlide>
-      <SwiperSlide><h1>Slide 1</h1></SwiperSlide>
-      <SwiperSlide><h1>Slide 1</h1></SwiperSlide>
->>>>>>> 834e07f4e9066de8291f5086f0abcffbeec7e831
     </Swiper>
-      <button class='rbtn1'><SlArrowRight /></button>
 
-    </>
+     
+     
+
+
+
+    </div>
   );
 };
 
